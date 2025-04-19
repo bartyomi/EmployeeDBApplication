3f18d1d62854b937670b6ad91d7b08fd7f03a15f
Артём Бурдейный <bartyomi@ya.ru>
2025-04-19 23:48:40 +0300

    Fix Form Design

--- EmployeeDBApplication/MainForm.Designer.cs
+++ EmployeeDBApplication/MainForm.Designer.cs
@@ -22,71 +22,88 @@ partial class MainForm
 
     private void InitializeComponent()
     {
-        this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
-        this.buttonAdd = new System.Windows.Forms.Button();
-        this.buttonEdit = new System.Windows.Forms.Button();
-        this.buttonDelete = new System.Windows.Forms.Button();
-        this.buttonRefresh = new System.Windows.Forms.Button();
-        ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
-        this.SuspendLayout();
-
+        dataGridViewEmployees = new DataGridView();
+        buttonAdd = new Button();
+        buttonEdit = new Button();
+        buttonDelete = new Button();
+        buttonRefresh = new Button();
+        ((System.ComponentModel.ISupportInitialize)dataGridViewEmployees).BeginInit();
+        SuspendLayout();
+        // 
         // dataGridViewEmployees
-        this.dataGridViewEmployees.AllowUserToAddRows = false;
-        this.dataGridViewEmployees.AllowUserToDeleteRows = false;
-        this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        this.dataGridViewEmployees.Location = new System.Drawing.Point(12, 12);
-        this.dataGridViewEmployees.Name = "dataGridViewEmployees";
-        this.dataGridViewEmployees.ReadOnly = true;
-        this.dataGridViewEmployees.Size = new System.Drawing.Size(760, 400);
-        this.dataGridViewEmployees.TabIndex = 0;
-
+        // 
+        dataGridViewEmployees.AllowUserToAddRows = false;
+        dataGridViewEmployees.AllowUserToDeleteRows = false;
+        dataGridViewEmployees.BackgroundColor = System.Drawing.SystemColors.Info;
+        dataGridViewEmployees.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        dataGridViewEmployees.GridColor = System.Drawing.SystemColors.ControlLightLight;
+        dataGridViewEmployees.Location = new System.Drawing.Point(16, 18);
+        dataGridViewEmployees.Margin = new Padding(4, 5, 4, 5);
+        dataGridViewEmployees.Name = "dataGridViewEmployees";
+        dataGridViewEmployees.ReadOnly = true;
+        dataGridViewEmployees.RowHeadersWidth = 51;
+        dataGridViewEmployees.Size = new System.Drawing.Size(1013, 615);
+        dataGridViewEmployees.TabIndex = 0;
+        // 
         // buttonAdd
-        this.buttonAdd.Location = new System.Drawing.Point(12, 418);
-        this.buttonAdd.Name = "buttonAdd";
-        this.buttonAdd.Size = new System.Drawing.Size(100, 30);
-        this.buttonAdd.TabIndex = 1;
-        this.buttonAdd.Text = "Add";
-        this.buttonAdd.UseVisualStyleBackColor = true;
-        this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
-
+        // 
+        buttonAdd.Location = new System.Drawing.Point(16, 643);
+        buttonAdd.Margin = new Padding(4, 5, 4, 5);
+        buttonAdd.Name = "buttonAdd";
+        buttonAdd.Size = new System.Drawing.Size(133, 46);
+        buttonAdd.TabIndex = 1;
+        buttonAdd.Text = "Add";
+        buttonAdd.UseVisualStyleBackColor = true;
+        buttonAdd.Click += buttonAdd_Click;
+        // 
         // buttonEdit
-        this.buttonEdit.Location = new System.Drawing.Point(118, 418);
-        this.buttonEdit.Name = "buttonEdit";
-        this.buttonEdit.Size = new System.Drawing.Size(100, 30);
-        this.buttonEdit.TabIndex = 2;
-        this.buttonEdit.Text = "Edit";
-        this.buttonEdit.UseVisualStyleBackColor = true;
-        this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
-
+        // 
+        buttonEdit.Location = new System.Drawing.Point(157, 643);
+        buttonEdit.Margin = new Padding(4, 5, 4, 5);
+        buttonEdit.Name = "buttonEdit";
+        buttonEdit.Size = new System.Drawing.Size(133, 46);
+        buttonEdit.TabIndex = 2;
+        buttonEdit.Text = "Edit";
+        buttonEdit.UseVisualStyleBackColor = true;
+        buttonEdit.Click += buttonEdit_Click;
+        // 
         // buttonDelete
-        this.buttonDelete.Location = new System.Drawing.Point(224, 418);
-        this.buttonDelete.Name = "buttonDelete";
-        this.buttonDelete.Size = new System.Drawing.Size(100, 30);
-        this.buttonDelete.TabIndex = 3;
-        this.buttonDelete.Text = "Delete";
-        this.buttonDelete.UseVisualStyleBackColor = true;
-        this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
-
+        // 
+        buttonDelete.Location = new System.Drawing.Point(299, 643);
+        buttonDelete.Margin = new Padding(4, 5, 4, 5);
+        buttonDelete.Name = "buttonDelete";
+        buttonDelete.Size = new System.Drawing.Size(133, 46);
+        buttonDelete.TabIndex = 3;
+        buttonDelete.Text = "Delete";
+        buttonDelete.UseVisualStyleBackColor = true;
+        buttonDelete.Click += buttonDelete_Click;
+        // 
         // buttonRefresh
-        this.buttonRefresh.Location = new System.Drawing.Point(672, 418);
-        this.buttonRefresh.Name = "buttonRefresh";
-        this.buttonRefresh.Size = new System.Drawing.Size(100, 30);
-        this.buttonRefresh.TabIndex = 4;
-        this.buttonRefresh.Text = "Refresh";
-        this.buttonRefresh.UseVisualStyleBackColor = true;
-        this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
-
+        // 
+        buttonRefresh.Location = new System.Drawing.Point(896, 643);
+        buttonRefresh.Margin = new Padding(4, 5, 4, 5);
+        buttonRefresh.Name = "buttonRefresh";
+        buttonRefresh.Size = new System.Drawing.Size(133, 46);
+        buttonRefresh.TabIndex = 4;
+        buttonRefresh.Text = "Refresh";
+        buttonRefresh.UseVisualStyleBackColor = true;
+        buttonRefresh.Click += buttonRefresh_Click;
+        // 
         // MainForm
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(784, 461);
-        this.Controls.Add(this.buttonRefresh);
-        this.Controls.Add(this.buttonDelete);
-        this.Controls.Add(this.buttonEdit);
-        this.Controls.Add(this.buttonAdd);
-        this.Controls.Add(this.dataGridViewEmployees);
-        this.Name = "MainForm";
-        this.Text = "Employee Management System";
-        this.ResumeLayout(false);
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = System.Drawing.SystemColors.Highlight;
+        ClientSize = new System.Drawing.Size(1045, 709);
+        Controls.Add(buttonRefresh);
+        Controls.Add(buttonDelete);
+        Controls.Add(buttonEdit);
+        Controls.Add(buttonAdd);
+        Controls.Add(dataGridViewEmployees);
+        Margin = new Padding(4, 5, 4, 5);
+        Name = "MainForm";
+        Text = "Employee Management System";
+        ((System.ComponentModel.ISupportInitialize)dataGridViewEmployees).EndInit();
+        ResumeLayout(false);
     }
 }
